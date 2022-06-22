fd752751e08f844992348e3a534094c0440e924a
ShivalikSharma98 <Shivaliksharma98@gmail.com>
2022-06-22 12:16:21 -0600

    rednering proper data from api

--- src/Components/Results/Results.js
+++ src/Components/Results/Results.js
@@ -1,21 +1,33 @@
 import React from 'react';
+import { Card } from 'react-bootstrap';
 
 function Results({ images }) {
 	if (!images.length) {
 		return <h2>No Images Found</h2>;
 	}
 	return (
-        <div className='gallery'>
-            {images.map((image) => (
-                <div key={image.id} className='art-image'>
-                    <h2>{image.title}</h2>
-                    <h5>{image.medium}</h5>
-                    <img src={image.images[0]?.baseimageurl} alt={image.images.alttext} />
-                    <p>{image.description}</p>
-                </div>
-            ))}
-        </div>
-    )
+		<div className='gallery'>
+			{images
+				.filter((image) => {
+					if (image?.images[0]?.baseimageurl) {
+						return true;
+					} else {
+						return false;
+					}
+				})
+				.map((image) => (
+					<div key={image.id} className='art-image'>
+						<h2>{image.title}</h2>
+						<h5>{image.people[0]?.name}</h5>
+						<img
+							src={image?.images[0]?.baseimageurl}
+							alt={image?.images[0]?.alttext}
+						/>
+						<p>{image.description}</p>
+					</div>
+				))}
+		</div>
+	);
 }
 
 export default Results;
